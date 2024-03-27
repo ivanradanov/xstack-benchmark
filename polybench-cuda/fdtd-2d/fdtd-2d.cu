28795b42f30189303d86d81eb535949d167b95bc
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-03-27 10:10:05 +0900

    Remove unused function in fdtd-2d

--- polybench-cuda/fdtd-2d/fdtd-2d.cu
+++ polybench-cuda/fdtd-2d/fdtd-2d.cu
@@ -147,38 +147,6 @@ void print_array(int nx,
   fprintf(stderr, "\n");
 }
 
-
-/* Main computational kernel. The whole function will be timed,
-   including the call and return. */
-static
-void kernel_fdtd_2d(int tmax,
-		    int nx,
-		    int ny,
-		    double ex[nx][ny],
-		    double ey[nx][ny],
-		    double hz[nx][ny],
-		    double _fict_[ny])
-{
-  int t, i, j;
-
-  for(t = 0; t < tmax; t++)
-    {
-      for (j = 0; j < ny; j++)
-	      ey[0][j] = _fict_[t];
-      for (i = 1; i < nx; i++)
-	      for (j = 0; j < ny; j++)
-	        ey[i][j] = ey[i][j] - 0.5*(hz[i][j]-hz[i-1][j]);
-      for (i = 0; i < nx; i++)
-	      for (j = 1; j < ny; j++)
-	        ex[i][j] = ex[i][j] - 0.5*(hz[i][j]-hz[i][j-1]);
-      for (i = 0; i < nx - 1; i++)
-	      for (j = 0; j < ny - 1; j++)
-	        hz[i][j] = hz[i][j] - 0.7*  (ex[i][j+1] - ex[i][j] +
-				       ey[i+1][j] - ey[i][j]);
-    }
-}
-
-
 int main(int argc, char** argv)
 {
   /* Retrieve problem size. */
