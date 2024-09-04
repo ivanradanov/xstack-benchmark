fa8da59713f5789712c1279e3a604ed8d5b4d3cc
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-09-04 18:05:35 +0900

    Initial covariance

--- polybench-cuda/covariance/covariance_gpu_noelle_manual.c
+++ polybench-cuda/covariance/covariance_gpu_noelle_manual.c
@@ -15,7 +15,7 @@ typedef unsigned char bool;
 #if defined(__GNUC__)
 #define  __ATTRIBUTELIST__(x) __attribute__(x)
 #else
-#define  __ATTRIBUTELIST__(x)
+#define  __ATTRIBUTELIST__(x)  
 #endif
 
 #ifdef _MSC_VER  /* Can only support "linkonce" vars with GCC */
@@ -155,7 +155,7 @@ int main(int argc, char ** argv) {
   cov = malloc(m * m * 8);
   _ZL10init_arrayiiPd(m, n, ((double*)data));
 ;
-#pragma omp target data map(to: data[0:n * m * 8]) map(alloc:mean[0:m * 8]) map(from: cov[0:m * m * 8])
+#pragma omp target data map(to: data[0:n * m * 8], mean[0:m * 8]) map(tofrom: cov[0:m * m * 8])
 {
   _ZL6kerneliiPdS_S_(m, n, ((double*)data), ((double*)cov), ((double*)mean));
 ;
@@ -175,10 +175,10 @@ void _ZL10init_arrayiiPd(uint32_t m, uint32_t n, double* data) {
   int64_t i;
   int64_t j;
 
-#pragma omp parallel for
-for(int64_t i = 0; i < n;   ++i){
+#pragma omp parallel for 
+for(int64_t i = 0; i < n;   i = i + 1){
 
-for(int64_t j = 0; j < m;   ++j){
+for(int64_t j = 0; j < m;   j = j + 1){
   data[(i * m + j)] = (((double)(i) * (double)(j)) / 1000);
 }
 }
@@ -187,50 +187,107 @@ for(int64_t j = 0; j < m;   ++j){
 
 
 void _ZL6kerneliiPdS_S_(uint32_t m, uint32_t n, double* data, double* cov, double* mean) {
+  struct l_struct_struct_OC_dim3 agg_2e_tmp;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp1;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp_2e_coerce;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp1_2e_coerce;    /* Address-exposed local */
   struct l_struct_struct_OC_dim3 block;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 grid;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp5;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp6;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp5_2e_coerce;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp6_2e_coerce;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp18;    /* Address-exposed local */
+  struct l_struct_struct_OC_dim3 agg_2e_tmp19;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp18_2e_coerce;    /* Address-exposed local */
+  struct l_unnamed_1 agg_2e_tmp19_2e_coerce;    /* Address-exposed local */
   int32_t call;
+  uint8_t* _1;
+  uint8_t* _2;
+  uint32_t i;
+  uint32_t j;
   int32_t call3;
   int32_t call4;
+  uint8_t* _3;
+  uint8_t* _4;
+  uint8_t* _5;
+  uint8_t* _6;
+  uint32_t k;
+  uint32_t l;
   int32_t call14;
   int32_t call17;
+  uint8_t* _7;
+  uint8_t* _8;
+  uint8_t* _9;
+  uint8_t* _10;
 
-  block.field0 = 8;
-  block.field1 = 32;
   call = _ZL10num_blocksii(m, 256);
-  call3 = _ZL10num_blocksii(n, block.field0);
-  call4 = _ZL10num_blocksii(m, block.field1);
-  call14 = _ZL10num_blocksii((m - 1), block.field0);
-  call17 = _ZL10num_blocksii((m - 1), block.field1);
-#pragma omp target teams distribute parallel for collapse(2) thread_limit(256)
+  agg_2e_tmp.field0 = call;
+  agg_2e_tmp.field1 = 1;
+  agg_2e_tmp.field2 = 1;
+  agg_2e_tmp1.field0 = 256;
+  agg_2e_tmp1.field1 = 1;
+  agg_2e_tmp1.field2 = 1;
+  memcpy(((uint8_t*)(&agg_2e_tmp_2e_coerce)), ((uint8_t*)(&agg_2e_tmp)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp1_2e_coerce)), ((uint8_t*)(&agg_2e_tmp1)), 12);
+#pragma omp target teams distribute
 
-for(int32_t i = 0; i < call;   ++i){
-//#pragma omp parallel for
+for(int32_t i = 0; i < call;   i = i + 1){
+#pragma omp parallel for
 
-for(int32_t j = 0; j < 256;   ++j){
+for(int32_t j = 0; j < 256;   j = j + 1){
 _Z11kernel_meaniiPdS_S__OC_1(m, n, data, cov, mean, call, 1, 1, 256, 1, 1, i, 0, 0, j, 0, 0);
 }
 }
-#pragma omp target teams distribute parallel for collapse(2) thread_limit(256)
-for(int32_t k = 0; k < call3;   ++k){
+  block.field0 = 8;
+  block.field1 = 32;
+  block.field2 = 1;
+  call3 = _ZL10num_blocksii(n, block.field0);
+  call4 = _ZL10num_blocksii(m, block.field1);
+  grid.field0 = call3;
+  grid.field1 = call4;
+  grid.field2 = 1;
+  memcpy(((uint8_t*)(&agg_2e_tmp5)), ((uint8_t*)(&block)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp6)), ((uint8_t*)(&grid)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp5_2e_coerce)), ((uint8_t*)(&agg_2e_tmp5)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp6_2e_coerce)), ((uint8_t*)(&agg_2e_tmp6)), 12);
+#pragma omp target teams distribute collapse(2)
 
-for(int32_t l = 0; l < call4;   ++l){
-//#pragma omp parallel for collapse(2)
-for(int32_t i = 0; i < 8;   ++i){
+for(int32_t i = 0; i < 8;   i = i + 1){
 
-for(int32_t j = 0; j < 32;   ++j){
+for(int32_t j = 0; j < 32;   j = j + 1){
+#pragma omp parallel for collapse(2)
+
+for(int32_t k = 0; k < call3;   k = k + 1){
+
+for(int32_t l = 0; l < call4;   l = l + 1){
 _Z13kernel_reduceiiPdS_S__OC_2(m, n, data, cov, mean, 8, 32, 1, call3, call4, 1, i, j, 0, k, l, 0);
 }
 }
 }
 }
-#pragma omp target teams distribute parallel for collapse(2) thread_limit(256)
-for(int32_t k = 0; k < call14;   ++k){
+  block.field0 = 8;
+  block.field1 = 32;
+  block.field2 = 1;
+  call14 = _ZL10num_blocksii((m - 1), block.field0);
+  call17 = _ZL10num_blocksii((m - 1), block.field1);
+  grid.field0 = call14;
+  grid.field1 = call17;
+  grid.field2 = 1;
+  memcpy(((uint8_t*)(&agg_2e_tmp18)), ((uint8_t*)(&block)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp19)), ((uint8_t*)(&grid)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp18_2e_coerce)), ((uint8_t*)(&agg_2e_tmp18)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp19_2e_coerce)), ((uint8_t*)(&agg_2e_tmp19)), 12);
+#pragma omp target teams distribute collapse(2)
 
-for(int32_t l = 0; l < call17;   ++l){
-//#pragma omp parallel for collapse(2)
-for(int32_t i = 0; i < 8;   ++i){
+for(int32_t i = 0; i < 8;   i = i + 1){
 
-for(int32_t j = 0; j < 32;   ++j){
+for(int32_t j = 0; j < 32;   j = j + 1){
+#pragma omp parallel for collapse(2)
+
+for(int32_t k = 0; k < call14;   k = k + 1){
+
+for(int32_t l = 0; l < call17;   l = l + 1){
 _Z10kernel_coviiPdS_S__OC_3(m, n, data, cov, mean, 8, 32, 1, call14, call17, 1, i, j, 0, k, l, 0);
 }
 }
@@ -246,9 +303,9 @@ void _ZL11print_arrayiPd(uint32_t m, double* cov) {
   int32_t call11;
 
 
-for(int64_t i = 0; i < m;   ++i){
+for(int64_t i = 0; i < m;   i = i + 1){
 
-for(int64_t j = 0; j < m;   ++j){
+for(int64_t j = 0; j < m;   j = j + 1){
   uint32_t call = fprintf(stderr, _OC_str, cov[(i * m + j)]);
   if ((int)(i * m + j) % (int)20 == 0) {
   fprintf(stderr, _OC_str_OC_1);
@@ -272,7 +329,7 @@ void _Z11kernel_meaniiPdS_S__OC_1(uint32_t m, uint32_t n, double* data, double* 
   if (j < m) {
   mean[j] = 0;
 
-for(int64_t i = 0; i < n;   ++i){
+for(int64_t i = 0; i < n;   i = i + 1){
   mean[j] = (mean[j] + data[(i * m + j)]);
 }
   mean[j] = (mean[j] / (double)(n));
@@ -307,7 +364,7 @@ void _Z10kernel_coviiPdS_S__OC_3(uint32_t m, uint32_t n, double* data, double* c
   if (j < m) {
   cov[(i * m + j)] = 0;
 
-for(int64_t k = 0; k < n;   ++k){
+for(int64_t k = 0; k < n;   k = k + 1){
   cov[(i * m + j)] = (cov[(i * m + j)] + (data[(k * m + i)] * data[(k * m + j)]));
 }
   cov[(i * m + j)] = (cov[(i * m + j)] / ((double)(n) - 1));
