026b847a9fc93081ab50c814942f93ad28943da7
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-09-09 18:35:47 +0900

    Initial nussinov amd

--- polybench-cuda/nussinov/nussinov_amd_manual.c
+++ polybench-cuda/nussinov/nussinov_amd_manual.c
@@ -15,7 +15,7 @@ typedef unsigned char bool;
 #if defined(__GNUC__)
 #define  __ATTRIBUTELIST__(x) __attribute__(x)
 #else
-#define  __ATTRIBUTELIST__(x)
+#define  __ATTRIBUTELIST__(x)  
 #endif
 
 #ifdef _MSC_VER  /* Can only support "linkonce" vars with GCC */
@@ -97,7 +97,7 @@ uint32_t _ZL10num_blocksii(uint32_t, uint32_t) __ATTRIBUTELIST__((noinline, noth
 uint32_t cudaConfigureCall(uint64_t, uint32_t, uint64_t, uint32_t, uint64_t, void*);
 uint32_t cudaMalloc(uint8_t**, uint64_t);
 double fmax(double, double);
-void _Z16kernel_max_scoreiPdS_S_i_OC_1(uint32_t, double*, double*, double*, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t) __ATTRIBUTELIST__((noinline, nothrow));
+void _Z16kernel_max_scoreiPdS_S_i_OC_1(uint32_t, double*, double*, double*, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t) __ATTRIBUTELIST__((noinline, nothrow));
 
 
 /* Global Variable Definitions and Initialization */
@@ -182,21 +182,17 @@ void _ZL10init_arrayiPdS_S_(uint32_t n, double* table, double* oldtable, double*
   int64_t i;
   uint64_t j;
 
-#pragma omp parallel
-{
-#pragma omp for //collapse(2)
-for(int64_t i = 0; i < n;   ++i){
-  seq[i] = ((double)(i) / (double)(n));
+#pragma omp parallel for 
+for(int64_t i = 0; i < n;   i = i + 1){
 
-for(int64_t j = 0; j < n;   ++j){
+for(int64_t j = 0; j < n;   j = j + 1){
   table[(i * n + j)] = (((double)(i) * (double)(j)) / (double)(n));
   oldtable[(i * n + j)] = (((double)(i) * (double)(j)) / (double)(n));
 }
 }
-//#pragma omp for
-//for(int64_t i = 0; i < n;   ++i){
-//  seq[i] = ((double)(i) / (double)(n));
-//}
+#pragma omp parallel for 
+for(int64_t i = 0; i < n;   i = i + 1){
+  seq[i] = ((double)(i) / (double)(n));
 }
   return;
 }
@@ -212,15 +208,23 @@ void _ZL6kerneliPdS_S_(uint32_t n, double* seq, double* table, double* oldtable)
   uint32_t k;
 
 
-for(int32_t w = n; w < (2 * n - 1);   ++w){
-  uint32_t call = _ZL10num_blocksii(n, 16);
-#pragma omp target teams distribute thread_limit(16)// parallel for collapse(2) thread_limit(1024)
+for(int32_t w = n; w < (2 * n - 1);   w = w + 1){
+  uint32_t call = _ZL10num_blocksii(n, 32);
+  agg_2e_tmp.field0 = call;
+  agg_2e_tmp.field1 = 1;
+  agg_2e_tmp.field2 = 1;
+  agg_2e_tmp1.field0 = 32;
+  agg_2e_tmp1.field1 = 1;
+  agg_2e_tmp1.field2 = 1;
+  memcpy(((uint8_t*)(&agg_2e_tmp_2e_coerce)), ((uint8_t*)(&agg_2e_tmp)), 12);
+  memcpy(((uint8_t*)(&agg_2e_tmp1_2e_coerce)), ((uint8_t*)(&agg_2e_tmp1)), 12);
+#pragma omp target teams distribute
 
-for(int32_t j = 0; j < call;   ++j){
+for(int32_t j = 0; j < call;   j = j + 1){
 #pragma omp parallel for
 
-for(int32_t k = 0; k < 16;   ++k){
-_Z16kernel_max_scoreiPdS_S_i_OC_1(n, seq, table, oldtable, w, call, 16, j, k);
+for(int32_t k = 0; k < 32;   k = k + 1){
+_Z16kernel_max_scoreiPdS_S_i_OC_1(n, seq, table, oldtable, w, call, 1, 1, 32, 1, 1, j, 0, 0, k, 0, 0);
 }
 }
 }
@@ -234,9 +238,9 @@ void _ZL11print_arrayiPd(uint32_t n, double* table) {
   int32_t call11;
 
 
-for(int64_t i = 0; i < n;   ++i){
+for(int64_t i = 0; i < n;   i = i + 1){
 
-for(int64_t j = 0; j < n;   ++j){
+for(int64_t j = 0; j < n;   j = j + 1){
   uint32_t call = fprintf(stderr, _OC_str, table[(i * n + j)]);
   if ((int)(i * n + j) % (int)20 == 0) {
   fprintf(stderr, _OC_str_OC_1);
@@ -252,7 +256,7 @@ uint32_t _ZL10num_blocksii(uint32_t num, uint32_t factor) {
 }
 
 
-void _Z16kernel_max_scoreiPdS_S_i_OC_1(uint32_t n, double* seq, double* table, double* oldtable, uint32_t w, uint32_t gridDim_2e_x, uint32_t blockDim_2e_x, uint32_t blockIdx_2e_x, uint32_t threadIdx_2e_x) {
+void _Z16kernel_max_scoreiPdS_S_i_OC_1(uint32_t n, double* seq, double* table, double* oldtable, uint32_t w, uint32_t gridDim_2e_x, uint32_t gridDim_2e_y, uint32_t gridDim_2e_z, uint32_t blockDim_2e_x, uint32_t blockDim_2e_y, uint32_t blockDim_2e_z, uint32_t blockIdx_2e_x, uint32_t blockIdx_2e_y, uint32_t blockIdx_2e_z, uint32_t threadIdx_2e_x, uint32_t threadIdx_2e_y, uint32_t threadIdx_2e_z) {
   uint32_t mul;
   int64_t j;
   int64_t i;
@@ -283,7 +287,7 @@ void _Z16kernel_max_scoreiPdS_S_i_OC_1(uint32_t n, double* seq, double* table, d
   }
   }
 
-for(int64_t k = (threadIdx_2e_x + n + mul - w); k < j;   ++k){
+for(int64_t k = (threadIdx_2e_x + n + mul - w); k < j;   k = k + 1){
   maximum = _ZL3maxdd(maximum, (table[(i * n + k)] + table[((k + 1) * n + j)]));
 }
   table[(i * n + j)] = maximum;
