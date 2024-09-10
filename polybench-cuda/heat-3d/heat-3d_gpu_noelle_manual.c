04dfc249890fb59ba7bd97e02b5590d468a78944
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-09-10 15:19:46 +0900

    opt heat-3d fix

--- polybench-cuda/heat-3d/heat-3d_gpu_noelle_manual.c
+++ polybench-cuda/heat-3d/heat-3d_gpu_noelle_manual.c
@@ -219,7 +219,7 @@ for(int32_t t = 1; t <= tsteps;   t = t + 1){
   memcpy(((uint8_t*)(&agg_2e_tmp5)), ((uint8_t*)(&block)), 12);
   memcpy(((uint8_t*)(&agg_2e_tmp_2e_coerce)), ((uint8_t*)(&agg_2e_tmp)), 12);
   memcpy(((uint8_t*)(&agg_2e_tmp5_2e_coerce)), ((uint8_t*)(&agg_2e_tmp5)), 12);
-#pragma omp target teams distribute parallel for collapse(5)
+#pragma omp target teams distribute parallel for collapse(2)
 
 for(int32_t j = 0; j < call;   j = j + 1){
 
@@ -240,7 +240,7 @@ _Z14kernel_stenciliPdS__OC_1(iter, A, B, call, call2, call4, 1, 8, 32, j, k, l, 
   memcpy(((uint8_t*)(&agg_2e_tmp8)), ((uint8_t*)(&block)), 12);
   memcpy(((uint8_t*)(&agg_2e_tmp7_2e_coerce)), ((uint8_t*)(&agg_2e_tmp7)), 12);
   memcpy(((uint8_t*)(&agg_2e_tmp8_2e_coerce)), ((uint8_t*)(&agg_2e_tmp8)), 12);
-#pragma omp target teams distribute parallel for collapse(5)
+#pragma omp target teams distribute parallel for collapse(2)
 
 for(int32_t j = 0; j < call;   j = j + 1){
 
